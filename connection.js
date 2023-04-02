fce23bf3a4fe94f5113f845e83f9bae927d5df41
JaredTrOr <trujillojaredalexander@gmail.com>
2023-04-02 17:57:50 -0600

    it was connected the backend with the cloud database

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.LINKMONGODB)
 .then(() => console.log('Database connection!!'))
 .catch(err => console.log(err));
